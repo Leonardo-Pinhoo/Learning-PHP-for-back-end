96ef35d90ad8036fff30c730ab7f7f158bddd666
Leonardo-Pinhoo <leonardo.rodriguesguidolim@gmail.com>
2025-08-14 15:59:34 -0300

    Firsts codes in PHP

---
+++ meu_site.com.br/arrays.php
@@ -0,0 +1,5 @@
+<?php
+    $frutas = ["Maçã", "Banana", "Laranja"];
+    echo $frutas[0]. "<br>";
+    echo $frutas[1]. "<br>";
+    echo $frutas[2]. "<br>";
---
+++ meu_site.com.br/exercise_arrays.php
@@ -0,0 +1,7 @@
+<?php
+    $frutas = ["Maçã ", "Banana ", "Laranja ", "Morango ", "Pera ", "Limão "];
+    for ($i == 1; $i <= 5; $i++ ){
+        echo "Fruta $i: ";
+        echo  $frutas[$i]. "<br>";
+    }
+    

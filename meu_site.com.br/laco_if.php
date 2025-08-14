05049c3896678d05973aa467ad88f16a7fe3f1c0
Leonardo-Pinhoo <leonardo.rodriguesguidolim@gmail.com>
2025-08-14 11:12:05 -0300

    Firsts codes in PHP

---
+++ meu_site.com.br/laco_if.php
@@ -0,0 +1,15 @@
+<?php
+    $resposta = "sim";
+    $pergunta = "Sua mãe deixou você ir ao Mc Donald's?";
+
+    if($resposta == "sim") {
+        echo "Let's go to the Mc Donald's";
+
+    } else{
+        echo "We don't going out today";
+    }
+
+
+
+
+?>

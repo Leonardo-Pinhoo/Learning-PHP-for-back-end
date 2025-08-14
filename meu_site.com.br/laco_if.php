5cb0fd7a41cb3b705b1cdeded5b6365471fef5ac
Leonardo-Pinhoo <leonardo.rodriguesguidolim@gmail.com>
2025-08-14 11:47:34 -0300

    Firsts codes in PHP

--- meu_site.com.br/laco_if.php
+++ meu_site.com.br/laco_if.php
@@ -8,8 +8,15 @@
     } else{
         echo "We don't going out today";
     }
+    echo "<br>";
 
+    $numero1 = 100;
+    $numero2 = 200;
 
-
-
+    if ($numero1 > $numero2){
+        echo "O numero 1 é maior que o numero 2";
+    } else{
+        echo "O numero 2 é maior que o numero 1";
+    }
+    
 ?>

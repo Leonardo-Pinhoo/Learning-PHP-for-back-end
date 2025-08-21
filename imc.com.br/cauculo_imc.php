4ec4128d39338312555183c861dd2d7788c0f1e3
Leonardo-Pinhoo <leonardo.rodriguesguidolim@gmail.com>
2025-08-21 15:44:15 -0300

    learning how to use PHP whith HTML

--- imc.com.br/cauculo_imc.php
+++ imc.com.br/cauculo_imc.php
@@ -6,10 +6,10 @@
     $imc = $peso / ($altura * $altura);
     echo "<p> <h2> Seu imc foi de ". round($imc, 2). "</h2> </p>";
 ?>    
-    <p><img src="https://sandermedicalcenter.com.br/wp-content/uploads/2023/08/imc.jpg" alt="O Galinho Chicken Little" width="350" height="400"></p>
+    <p><img src="https://veja.abril.com.br/wp-content/uploads/2024/10/arte-IMC-digital-2917.jpg?crop=1&resize=1212,909" alt="Tabela dos valores de imc" width="350" height="450"></p>
 <?php
     if($imc < 18.5){
-        echo "<p> <h2>De acordo com o seu imc de ". round($imc, 2). " você está em subpeso </h2></p>";
+        echo "<p> <h2>De acordo com o seu imc de ". round($imc, 2). " você está abaixo do peso </h2></p>";
     } elseif($imc >= 18.5 && $imc < 24.9){
         echo "<p> <h2>De acordo com o seu imc de ". round($imc, 2). " você está com o peso ideal </h2></p>";
     } elseif($imc >= 24.9 && $imc < 29.9){
